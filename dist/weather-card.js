d4b4ed7ac2b5da274edc7ec7ba0e4b00e3ed8c01
steven-herr <99555072+steven-herr@users.noreply.github.com>
2022-02-16 21:33:56 -0500

    Update weather-card.js

--- dist/weather-card.js
+++ dist/weather-card.js
@@ -168,7 +168,7 @@ class WeatherCard extends LitElement {
           style="background: none, url('${this.getWeatherIcon(
             stateObj.state.toLowerCase(),
             this.hass.states["sun.sun"]
-          )}') no-repeat; background-size: contain; image-rendering: -webkit-optimize-contrast; transform: translateZ(0);"
+          )}') no-repeat; background-size: contain; image-rendering: -webkit-optimize-contrast; transform: translateZ(0); -webkit-transform: scale(1); transform: scale(1)"
           >${stateObj.state}
         </span>
         ${this._config.name
